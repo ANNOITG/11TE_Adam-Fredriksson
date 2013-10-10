998dfad99d1b6063d0d23ee44c034bc5656a0db1
addebooi <adam@fredrikson.net>
2013-10-10 13:14:24 +0200

    changed

--- Programering/004 arv_praktiskt/004 arv_praktiskt/Program.cs
+++ Programering/004 arv_praktiskt/004 arv_praktiskt/Program.cs
@@ -6,38 +6,166 @@ using System.Threading.Tasks;
                                         /*Klasser skall placeras så att det är en klass per fil (med samma namn). Din kommentering kan förbättras.*/
 namespace _004_arv_praktiskt {
     class Program {
-        
+        //listorna där elever samt lärare sparas
+        public static List<Elev> elevLista = new List<Elev>();
+        public static List<Lärare> lärarLista = new List<Lärare>();
+         
         static void Main(string[] args) {
+            /*
             Console.WriteLine("Välkommen till arbetsmiljön! klicka på valfri knapp");
+            Program program = new Program();
+            elevLista.Add(new Elev("Gabbe", "1337", "snopp", "13371337", "11Te", new Kurs("Programering", "k")));
+            elevLista.Add(new Elev("Nuben", "1337", "snopp", "13371337", "11Te", new Kurs("Programering", "k")));
+            program.Introduktion();
+             */
+            
+        }
+        /*
+        public void Introduktion() {
             Console.ReadKey();
+            Console.Clear();
             Console.WriteLine("1. Läs av lärare i skolan\n2. Läs av elever i skolan\n" +
-                "3. Lägg in lärare i systemet\n4. Lägg in elev i systemet\n5. Tryck på 5 för att avsluta programmet");
-            
-
-            Kurs Kursen = new Kurs("Programering", "A+");
-            List<Elev> elevLista = new List<Elev>();
-            List<Lärare> lärarLista = new List<Lärare>();
-            
-            lärarLista.Add(new Lärare("Anno", "1337", "Landet", "13371337", "500", new lärarKurser("programering")));
-            elevLista.Add(new Elev("addebooi", "1337", "Bajsgatan32", "13371337", "11te", new Kurs("Programering","A+")));
-            elevLista.Add(new Elev("Jockebooi", "1336", "Korvgatan92", "SwagNummer", "noob", new Kurs("Svenska1", "F+")));
-            for (int i = 0; i < elevLista.Count; i++) {
-                elevLista[i].setKurs(new Kurs("Svenska2", "C+"));
-                Console.WriteLine(elevLista[i].ToString());
-                Console.WriteLine("----------------------------------------------------");
+                "3. Lägg in lärare i systemet\n4. Lägg in elev i systemet\n" +
+                "5. Redigera Elev\n6. Redigera Lärare\n"+
+                "7. avsluta programmet");
+            string key = Console.ReadLine();
+            Console.Clear();
+            if (key == "1") {
+                skrivUtLärare();
             }
-
-            for (int i = 0; i < lärarLista.Count; i++) {
-                lärarLista[i].setKurs(new lärarKurser("WebbserverProgramering"));
-                Console.WriteLine(lärarLista[i].ToString());
+            if (key == "2") {
+                skrivUtElever();
             }
-            
+            if (key == "3") {
+                läggTillLärare();
+            }
+            if (key == "4") {
+                läggTillElev();
+            }
+            if (key == "5") {
+                redigeraElev();
+            }
+            if (key == "6") {
+                redigeraLärare();
+            }
             Console.ReadKey();
         }
-
         public void skrivUtLärare() {
+            if (lärarLista.Count() != 0) {
+                for (int i = 0; i < lärarLista.Count; i++) {
+                    skrivUtLärarePåIndex(i);
+                }
+            } else {
+                Console.WriteLine("Var god lägg till en lärare!");
+                Introduktion();
+            }
         }
         public void skrivUtElever() {
+            if (elevLista.Count() != 0) {
+                for (int i = 0; i < elevLista.Count; i++) {
+                    skrivUtElevPåIndex(i);
+                    
+                }
+            } else {
+                Console.WriteLine("Var god lägg till en Elev!");
+                Introduktion();
+            }
+        }
+        public void redigeraElev() {
+            if (elevLista.Count() != 0) {
+                int elevVald = -1;
+                for (int i = 0; i < elevLista.Count(); i++) {
+         *          list<kurs> tempList = new 
+                    Console.WriteLine(i + ". " + elevLista[i].getNamn() + " " + elevLista[i].getKlass());
+                }
+                while(elevVald<0 || elevVald>elevLista.Count()){
+                    try {
+                        elevVald = Convert.ToInt32(Console.ReadLine());
+                        Console.Clear();
+                        
+                    } catch {
+                        Console.WriteLine("Eleven du valde är inte ett index eller existerar inte!, var god skriv indexet!");
+                    }
+                }
+                Console.Clear();
+                Console.WriteLine(elevVald);
+                if (elevVald != -1 && elevVald < elevLista.Count()) {
+                    string ändringAvElev = "";
+                    Console.WriteLine("Vad vill du redigera på eleven: " + elevLista[elevVald].getNamn());
+                    Console.WriteLine("1. namn\n2. pNr\n3. Adress\n4. teleNr\n5. klass\n6. kurs");
+
+                    string key = Console.ReadLine();
+                    Console.Clear();
+                    if (key == "1") {
+                        Console.Write("Namn: ");
+                        ändringAvElev = Console.ReadLine();
+                        elevLista[elevVald].setNamn(ändringAvElev);
+                        Console.Clear();
+
+                        Console.WriteLine("Namnet är nu ändrat! Tryck på valfri knapp för att komma tillbaka till menyn");
+                    }
+                    if (key == "2") {
+                        Console.Write("Personnummer: ");
+                        ändringAvElev = Console.ReadLine();
+                        elevLista[elevVald].setPnr(ändringAvElev);
+                        Console.Clear();
+                        Console.WriteLine("Personnummret är nu ändrat! Tryck på valfri knapp för att komma tillbaka till menyn");
+                    }
+                    if (key == "3") {
+                        Console.Write("Adress: ");
+                        ändringAvElev = Console.ReadLine();
+                        elevLista[elevVald].setAdress(ändringAvElev);
+                        Console.Clear();
+                        Console.WriteLine("Adressen är nu ändrat! Tryck på valfri knapp för att komma tillbaka till menyn");
+                    }
+                    if (key == "4") {
+                        Console.Write("Telenr: ");
+                        ändringAvElev = Console.ReadLine();
+                        elevLista[elevVald].setTeleNr(ändringAvElev);
+                        Console.Clear();
+                        Console.WriteLine("Telefonnummret är nu ändrat! Tryck på valfri knapp för att komma tillbaka till menyn");
+                    }
+                    if (key == "5") {
+                        Console.Write("Klass: ");
+                        ändringAvElev = Console.ReadLine();
+                        elevLista[elevVald].setKlass(ändringAvElev);
+                        Console.Clear();
+                        Console.WriteLine("Klassen är nu ändrat! Tryck på valfri knapp för att komma tillbaka till menyn");
+                    }
+                    if (key == "6") {
+                        for (int i = 0; i < elevLista[elevVald].getKurs().Count(); i++) {
+                            Console.WriteLine(i + ". " + elevLista[elevVald].getKurs()[i].Kursen);
+                        }
+                        Console.ReadKey();
+                    }
+                    Introduktion();
+                } 
+
+                else {
+                    Console.WriteLine("Eleven du skrev finns inte i registret");
+                    Introduktion();
+                }
+
+            } else {
+                Console.WriteLine("Var god lägg till en Elev!");
+                Introduktion();
+            }
+        }
+        public void skrivUtElevPåIndex(int index) {
+            Console.WriteLine(elevLista[index].ToString());
+            Console.WriteLine("----------------------------------------------------");
+        }
+        public void skrivUtLärarePåIndex(int index) {
+            Console.WriteLine(lärarLista[index].ToString());
+            Console.WriteLine("----------------------------------------------------");
+        }
+        public void redigeraLärare() {
+            if (lärarLista.Count() != 0) {
+                //Redigera elev listan
+            } else {
+                Console.WriteLine("Var god lägg till en lärare!");
+                Introduktion();
+            }
         }
         public Lärare läggTillLärare() {
             Lärare lärare = new Lärare("","","","","",new lärarKurser(""));
@@ -46,8 +174,8 @@ namespace _004_arv_praktiskt {
         public Elev läggTillElev() {
             Elev elev = new Elev("","","","","",new Kurs("",""));
             return elev;
-        }
-        
+        } 
+         */
     }
     class Person {
         private string namn;
@@ -67,8 +195,6 @@ namespace _004_arv_praktiskt {
         public string getAdress() { return adress; }
         public void setAdress(string adress) { this.adress = adress; }
     }
-
-
     class Lärare : Person {
         private string Lön;
         private List<lärarKurser> Kurs = new List<lärarKurser>();
@@ -106,12 +232,29 @@ namespace _004_arv_praktiskt {
             setKurs(kurs);
         }
     }
-
     class Elev : Person {
         private List<Kurs> Kurs = new List<Kurs>();
         private string Klass;
 
         public List<Kurs> getKurs() { return Kurs; }
+        public List<Kurs> getKursUtanBetyg() {
+            List<Kurs> tempList = new List<Kurs>();
+            for (int i = 0; i < Kurs.Count(); i++) {
+                if (Kurs[i].Betyg == "" || Kurs[i].Betyg == null) {
+                    tempList.Add(Kurs[i]);
+                }
+            }
+            return tempList;
+        }
+        public List<Kurs> getKursMedBetyg() {
+            List<Kurs> tempList = new List<Kurs>();
+            for (int i = 0; i < Kurs.Count(); i++) {
+                if (Kurs[i].Betyg != "") {
+                    tempList.Add(Kurs[i]);
+                }
+            }
+            return tempList;
+        }
         public void setKurs(Kurs Kurs) {
             this.Kurs.Add(Kurs);
         }
@@ -122,7 +265,6 @@ namespace _004_arv_praktiskt {
         public void fåBetygAvLärare(Kurs Kurs) {
             setKurs(Kurs);
         }
-
         public override string ToString() {
             List<Kurs> Kurs = getKurs();
             string kursString = "";
@@ -146,13 +288,11 @@ namespace _004_arv_praktiskt {
             setKlass(Klass);
         }
     }
-
     public class Kurs {
         public string Kursen { get; set; }
         public string Betyg { get; set; }
 
         public Kurs(string kurs, string betyg) {
-           
             this.Betyg = betyg;
             this.Kursen = kurs;
         }
